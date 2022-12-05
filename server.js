527ecc9f608f3866ca6262e3d419172cd7387c3a
Chris Williams <voodootikigod@gmail.com>
2022-12-05 09:53:19 -0500

    :lipstick: eslint adjustments.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const apn = require("./lib/apn");
+const express = require('express');
+const apn = require('./lib/apn');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,27 +7,27 @@ const USERNAME = process.env.APN_USERNAME;
 const PASSWORD = process.env.APN_PASSWORD;
 
 
-app.get("/opportunities", async(req, res) => {
-  const opps = await apn.opportunities()
+app.get('/opportunities', async (req, res) => {
+  const opps = await apn.opportunities();
   res.send(opps);
 });
 
-app.get("certifications", async (req, res)=>{
+app.get('certifications', async (req, res) => {
   const certifications = await apn.certifications();
   res.send(certifications);
 });
 
 
-let start = async () => {
+const start = async () => {
   await apn.init(USERNAME, PASSWORD, {
     headless: true,
     args: [
-        '--disable-gpu',
-        '--no-sandbox',
+      '--disable-gpu',
+      '--no-sandbox',
     ],
-    executablePath: 'google-chrome'
+    executablePath: 'google-chrome',
   });
-  app.listen(PORT, () => logger.log(`Listening on port ${PORT}`))
+  app.listen(PORT, () => logger.log(`Listening on port ${PORT}`));
 };
 
 start();
